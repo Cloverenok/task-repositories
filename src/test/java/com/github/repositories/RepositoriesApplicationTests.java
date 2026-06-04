0d0a359a84f0c2c145ee80f019702fb1e59d2300
Cloverenok <152108031+Cloverenok@users.noreply.github.com>
2026-06-04 10:38:30 +0200

    Initial commit

---
+++ src/test/java/com/github/repositories/RepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
